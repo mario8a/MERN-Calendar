885140e6102ce9174124b0e9a6bd8cb8ec5482f9
mario8a <mario8ato@hotmail.com>
2020-10-12 22:17:38 -0500

    Diseño del calendario

---
+++ src/CalendarApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
+
+
+export const CalendarApp = () => {
+   return (
+      <div>
+        <AppRouter/>
+
+        
+      </div>
+   )
+}
---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Navbar = () => {
+   return (
+      <div className="navbar navbar-dark bg-dark mb-4">
+         <span className="navbar-brand">
+            Mario Gonzalo
+         </span>
+
+         <button className="btn btn-danger">
+            <i className="fas fa-sign-out-alt"></i>
+            <span> Salir</span>
+         </button>
+      </div>
+   )
+}
